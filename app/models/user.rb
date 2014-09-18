f0abadaac939dd2d6a515a414672ac0213ff39a4
Benjamin Tiddle <benjamin@intheflow.com>
2014-09-18 13:53:03 -0700

    Chapter 8: Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
+  before_create :create_remember_token
+
 
   # This one has the problem that multiple periods (tom@tom.....com)
   # revaluated to valid when it should be negative.
@@ -12,4 +14,19 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -112,4 +113,11 @@ describe User do
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
+
 end
